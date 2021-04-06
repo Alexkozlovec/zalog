9a9a36a64003fce24659e47efab7ce7e3523964d
Aleksandr Kozlovets <kozlovetsalexandr@gmail.com>
2021-04-07 00:45:32 +0300

    fix styles and layout

--- dist/scripts/animations.js
+++ dist/scripts/animations.js
@@ -1,25 +1,28 @@
-// Анимаций нет в тз, но я чуть-чуть добавил)
-// Без библиотек и по скроллу. Весит совсем ничего, а пользователю приятно)
+// Добавил немного анимаций по скроллу)
+// Без библиотек. Весит совсем ничего, а скроллить интереснее.
 
-// Используется Intersection Observer API https://developer.mozilla.org/ru/docs/Web/API/Intersection_Observer_API
+// Использую Intersection Observer API https://developer.mozilla.org/ru/docs/Web/API/Intersection_Observer_API
 // Сами анимации в отдельном css файле css/animations.css или
 // где-то внизу итогового файла)
 
 const elementsToAnimate = document.querySelectorAll("[data-reveal]");
-let threshold = 0;
+
+const animationClass = "animated";
+const hidingElementClass = "hidden";
 
 // Процент видимости элемента во вьюпорте, при котором начать анимацию
 // Уменьшаю на больших разрешениях
+let threshold = 0;
 if(window.innerWidth < 992) {
   threshold = 0.7;
 } else {
   threshold = 0.4;
 }
 
-// css: .hidden {opacity: 0}
+// CSS: .hidden {opacity: 0}
 const addFullOpacityClass = (elements) => {
   elements.forEach(el => {
-    el.classList.add("hidden");
+    el.classList.add(hidingElementClass);
   });
 }
 
@@ -29,21 +32,37 @@ const setAnimationDelay = (el) => {
 }
 
 const addAnimationClass = (el) => {
-  el.classList.add("animating");
+  el.classList.add(animationClass);
 }
 
+// Убирает анимационные классы
+const removeAnimationClasses = (el) => {
+  el.classList.remove(hidingElementClass, animationClass);
+}
+
+const animateElement = (el) => {
+  setAnimationDelay(el);
+  addAnimationClass(el);
+
+  el.addEventListener("animationend", () => {
+    removeAnimationClasses(el);
+  });
+}
+
+// Отслеживает позицию полученных элементов и анимирует в нужный момент
 const observer = new IntersectionObserver((entries) => {
   entries.forEach(entry => {
     if(entry.isIntersecting) {
-      setAnimationDelay(entry.target);
-      addAnimationClass(entry.target);
+      animateElement(entry.target);
       observer.unobserve(entry.target);
     }
   })
 }, {threshold: threshold});
 
+// Прячет элементы перед анимацией
 addFullOpacityClass(elementsToAnimate);
 
+// Запускает наблюдатель
 elementsToAnimate.forEach(el => {
   observer.observe(el);
 });
